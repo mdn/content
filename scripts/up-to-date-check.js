50624bc0ed7d41393a0a58b5349c6fc0eb4d5ea8
Peter Bengtsson <mail@peterbe.com>
2021-01-11 15:44:28 -0500

    feedbacked

--- scripts/up-to-date-check.js
+++ scripts/up-to-date-check.js
@@ -6,9 +6,10 @@ const installedYariVersion = yariPackageJson.version;
 
 if (installedYariVersion < availableYariVersion) {
   console.log(
-    `The version of @mdn/yari that's installed (${installedYariVersion}) ` +
-      `is older than the version that is in package.json (${availableYariVersion}). ` +
-      "Please run: `yarn install` to be up-to-date."
+    `The installed version of Yari (${installedYariVersion}) is older than ` +
+      `the version referenced in package.json (${availableYariVersion}). ` +
+      `Please make sure your main git branch is up-to-date with ` +
+      `https://github.com/mdn/content/, then run yarn install to ` +
+      `install the latest Yari.`
   );
-  console.log("Also, make sure your `main` git branch is up to date.");
 }
