41fc41fe41e7f7059be31a4c27412ef315550b2f
Michael Glenn <mbg198618@gmail.com>
2021-07-15 03:27:58 -0500

    Create acception-as-arg.py

---
+++ acception-as-arg.py
@@ -0,0 +1,8 @@
+function sayHello() {
+   return "Hello, ";
+}
+function greeting(helloMessage, World) {
+  console.log(helloMessage(Hello) + World);
+}
+// Pass `sayHello` as an argument to `greeting` function
+greeting(sayHello, "JavaScript!");
