6b29f86d5f5ea0fe8d2af6f36a1f236e40c31399
ASHOKREDDDY1234 <95851968+ASHOKREDDDY1234@users.noreply.github.com>
2022-01-27 17:37:13 +0530

    Create envconfig.js

---
+++ envconfig.js
@@ -0,0 +1,6 @@
+(function(window) {
+      window["envconfig"] = window["envconfig"] || {};
+
+      // Environment variables
+      window["envconfig"]["apiurl"] = "http://localhost:8080/api";
+})(this);
