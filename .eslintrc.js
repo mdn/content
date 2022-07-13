578aa3ed243517d7275c99c9e1929fd718a07f0b
Lion Ralfs <lion.ralfs@gmail.com>
2022-07-13 21:09:52 +0200

    reset eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,3 @@
 module.exports = {
-  extends: [
-    // "plugin:json/recommended"
-    "plugin:markdown/recommended",
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    // allows top-level await among other things
-    sourceType: "module",
-  },
-  env: {
-    es6: true,
-    browser: true,
-    node: true,
-  },
+  extends: ["plugin:json/recommended"],
 };
