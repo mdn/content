4a4abfe8a9f85868d68bc7e7734d91d143a4cf07
OnkarRuikar <87750369+OnkarRuikar@users.noreply.github.com>
2024-09-06 14:07:02 +0530

    address review comments

--- scripts/linkify-logs.js
+++ scripts/linkify-logs.js
@@ -6,11 +6,13 @@
 const logs = process.argv[2];
 
 console.log(
-  logs.replaceAll(/(files\/.*?\/index.md):?(\d+)?/g, (_, path, lineNo) => {
-    let link = `[${path}](https://github.com/mdn/content/blob/main/${path}?plain=1`;
-    if (lineNo) {
-      return link + `#L${lineNo}):${lineNo}`;
-    }
-    return link + `)`;
-  }),
+  logs.replaceAll(
+    /(files\/.*?\/index.md):?(\d+)?:?(\d+)?/g,
+    (_, path, lineNo, columnNo) => {
+      if (lineNo) {
+        return `[${path}:${lineNo}:${columnNo}](https://github.com/mdn/content/blob/main/${path}?plain=1#L${lineNo})`;
+      }
+      return `[${path}](https://github.com/mdn/content/blob/main/${path}?plain=1)`;
+    },
+  ),
 );
