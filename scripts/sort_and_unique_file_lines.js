b60649a103a60eff93f23a9309247dc67775ce55
Joshua Chen <sidachen2003@gmail.com>
2024-10-29 09:11:21 -0400

    Fix action

--- scripts/sort_and_unique_file_lines.js
+++ scripts/sort_and_unique_file_lines.js
@@ -1,21 +1,30 @@
 import fs from "node:fs";
 
 const check = process.argv[2] === "--check";
-const filePaths = process.argv.slice(check ? 3 : 2);
+const inputPaths = process.argv.slice(check ? 3 : 2);
 
-if (!filePaths.length) {
+if (!inputPaths.length) {
   console.error(`No file paths provided.`);
   console.warn(
-    "Usage:\n\tnode scripts/sort_and_unique_file_lines.js [--check] [filePaths...]",
+    "Usage:\n\tnode scripts/sort_and_unique_file_lines.js [--check] [inputPaths...]",
   );
   process.exit(1);
 } else {
-  const invalidFiles = filePaths.filter((filePath) => !fs.existsSync(filePath));
+  const invalidFiles = inputPaths.filter(
+    (filePath) => !fs.existsSync(filePath),
+  );
   if (invalidFiles.length) {
     console.error(`Invalid file paths: ${invalidFiles.join(", ")}`);
     process.exit(1);
   }
 }
+
+const filePaths = inputPaths.flatMap((arg) => {
+  if (fs.statSync(arg).isDirectory()) {
+    return fs.readdirSync(arg).map((file) => `${arg}/${file}`);
+  }
+  return arg;
+});
 
 const equalsIgnoreCase = (a, b) => a?.toLowerCase() === b?.toLowerCase();
 
