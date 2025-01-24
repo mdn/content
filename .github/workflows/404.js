d177052314838a24f425fbbdb86a5f01b50046f2
OnkarRuikar <87750369+OnkarRuikar@users.noreply.github.com>
2025-01-24 10:32:05 +0530

    apply review comments

--- .github/workflows/404.js
+++ .github/workflows/404.js
@@ -1,7 +1,14 @@
+function getContentsAfterHost() {
+  const full = location.href;
+  const path = location.pathname;
+
+  return full.substring(full.indexOf(path));
+}
+
 window.addEventListener("DOMContentLoaded", () => {
-  if (document.location.hostname.endsWith("content.dev.mdn.mozit.cloud")) {
+  if (location.hostname.endsWith("content.dev.mdn.mozit.cloud")) {
     const link = document.createElement("a");
-    link.href = `https://developer.mozilla.org${location.pathname}`;
+    link.href = `https://developer.mozilla.org${getContentsAfterHost()}`;
     link.textContent = "View on MDN production server";
     document.body.appendChild(link);
   }
